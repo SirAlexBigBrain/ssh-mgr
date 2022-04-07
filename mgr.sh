312306e2f0730875ec6be2e3d4e8003215b8f2a0
Hardwaregore <hardwaregore101@gmail.com>
2022-04-07 16:50:06 -0500

    Update mgr.sh

--- mgr.sh
+++ mgr.sh
@@ -10,12 +10,12 @@ fi
 
 
 if [ $1 = store ]; then
-echo $RANDOM | md5sum | echo $2 > passwords.txt
+echo $RANDOM | md5sum > passwords.txt && echo $2 > passwords.txt
 
 fi
 
 if [ $1 = add ]; then
-echo $2 > passwords.txt | echo $3 > passwords.txt
+echo $2 > passwords.txt && echo $3 > passwords.txt
 fi
 
 if [ $1 = --read ]; then
