e4d9268297ec329531d39b908b925c26c17b9e26
Hardwaregore <hardwaregore101@gmail.com>
2022-04-07 14:38:34 -0500

    Update mgr.sh

--- mgr.sh
+++ mgr.sh
@@ -8,11 +8,18 @@ if [ $1 = --help ]; then
 echo ""
 fi
 
+if [ $1 = -h ]; then
+echo ""
+fi
+
 if [ $1 = store]; then
 fi
 
 if [ $1 = add ]; then
 fi
 
-if [ $1 = read ]; then
+if [ $1 = --read ]; then
 if
+
+if [ $1 = -r ]; then
+fi
