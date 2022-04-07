e172ec5fa52dd83b44b42010ceedc2735fae7bdf
Hardwaregore <hardwaregore101@gmail.com>
2022-04-07 14:35:32 -0500

    Update mgr.sh

--- mgr.sh
+++ mgr.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+if [ $1 = generate ]; then 
 echo $RANDOM | md5sum | head -c 20; echo;
+fi
+
+if [ $1 = --help ]; then
+echo ""
+fi
+
+if [ $1 = store]; then
+fi
+
+if [ $1 = add ]; then
+fi
+
+if [ $1 = read ]; then
+if
